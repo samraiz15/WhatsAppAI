2c6859d1fdb648c080528a75fbe98425874710fd
samraiz15 <samraizrealtor@gmail.com>
2026-09-21 15:16:24 +0000

    add demo mode configuration

---
+++ demo-config.js
@@ -0,0 +1,13 @@
+module.exports = Object.freeze({
+  demoMode: true,
+
+  localTimeoutMs: 15000,
+  fallbackTimeoutMs: 20000,
+
+  maxMessageLength: 2000,
+  maxMessagesPerMinute: 10,
+
+  maxConcurrentLocalAi: 1,
+
+  loggingEnabled: true
+});
